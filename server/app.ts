02cfb7cf4c2971b1908bda30cf476ae794cbcdbb
Ivan Verdugo <ivan270@gmail.com>
2023-10-08 19:17:13 -0300

    build: update build script to build both server and front

--- server/app.ts
+++ server/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import tasksRoutes from './routes';
+import path from 'path';
 
 const app = express();
 
@@ -10,5 +11,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 app.use('/api', tasksRoutes);
+
+app.use(express.static(path.join(__dirname, '..', '..', 'dist')));
 
 export default app;
